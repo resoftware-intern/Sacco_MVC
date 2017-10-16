cebb1e50f8a35ddf1aa64c78b5aefd2d2b577589
Sacco <s.vaes@resoftware.nl>
2017-10-16 14:28:08 +0200

    Added View

--- ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+            /*
             //Redirectrusult en Contentresult zijn klassen in MVC die een result/response geven.
             //Deze erfen beide van ActionResult, dus deze wordt bijna altijd meegegeven bij ActionMethods.
             if(DateTime.Today.DayOfWeek == DayOfWeek.Monday)
@@ -21,6 +22,12 @@ namespace ComicBookGallery.Controllers
             {
                 Content = "Hello from the comic book controller!"
             };
+            */
+
+            //Een view returnen!!!
+            return View();
+
+
         }
     }
 }
