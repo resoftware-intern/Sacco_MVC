d6a11c316b10996be5d4530f3fafd2209b9f7fad
Sacco <s.vaes@resoftware.nl>
2017-10-16 13:51:15 +0200

    Updated Action Method

--- ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,6 +8,19 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-
+        public ActionResult Detail()
+        {
+            //Redirectrusult en Contentresult zijn klassen in MVC die een result/response geven.
+            //Deze erfen beide van ActionResult, dus deze wordt bijna altijd meegegeven bij ActionMethods.
+            if(DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                //return new RedirectResult("/"); is hetzelfde als onderstaand code:
+                return Redirect("/");
+            }
+            return new ContentResult()
+            {
+                Content = "Hello from the comic book controller!"
+            };
+        }
     }
 }
